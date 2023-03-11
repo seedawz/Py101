85eea7f2924e0bd038a3b59ab6ccdda24b442f1c
seedawz <126739953+seedawz@users.noreply.github.com>
2023-03-11 15:30:01 +0700

    Conversion
    test conversion in python

---
+++ conversion.py
@@ -0,0 +1,17 @@
+x = 10 #int
+y = 3.14 #float
+z = "20" #str
+
+answer = x+y
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(answer)
+
+#convert
+print(x+int(z)) #result 20=x+z
+print(str(x)+z) #result x+z= 1020
+print(int(y))   #result = 3
+
+print('day '+z) #result day 20
