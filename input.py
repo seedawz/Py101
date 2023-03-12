65569385c30a90048f8bbba94a78ca768a8c5743
seedawz <126739953+seedawz@users.noreply.github.com>
2023-03-12 20:32:15 +0700

    input data from user

---
+++ input.py
@@ -0,0 +1,19 @@
+# function input
+
+# input
+fname = input("ชื่อของท่าน ") #รับค่าที่ป้อนแล้วเก็บลงตัวแปร fname
+lname = input("นามสกุลของท่าน")
+#output
+print("ชื่อ = "+fname)
+print("นามสกุล = "+lname)
+
+#input อยากบวกลบเลข
+x = input('เลขตัวที่ 1 :')
+y = input('เลขตัวที่ 2 :')
+
+#process
+z= int(x)+int(y)
+
+#output
+print(z)
+print(int(x)+int(y))
