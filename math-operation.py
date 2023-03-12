df82b388acb7c5939464594e9f66c87a7550c3e3
seedawz <126739953+seedawz@users.noreply.github.com>
2023-03-12 21:02:59 +0700

    Experiment with mathematical operators.

---
+++ math-operation.py
@@ -0,0 +1,15 @@
+# operation / operand
+# A+B 
+# A, B => operand  
+# +    => operation
+
+x = 20
+y = 3
+
+print('บวกเลข', x+y)
+print('ลบเลข', x-y)
+print('คูณเลข', x*y)
+print('หารเลข', x/y)
+print('ยกกำลัง', x**y)
+print('หารปัดเศษ', x//y)
+print('หาเศษ', x%y)
